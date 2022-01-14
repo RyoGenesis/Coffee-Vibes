8f28086575ee9590873a4b829f39376d215de0e1
Sendiawan Muljono <sendiawanmuljono@gmail.com>
2022-01-14 20:19:37 +0700

    Update ProductHandler.java

--- src/handlers/ProductHandler.java
+++ src/handlers/ProductHandler.java
@@ -33,14 +33,111 @@ public class ProductHandler {
 	
 	//**UNFINISHED**
 	public Product getProduct(int productID) {
+		return product.getProduct(productID);
+	}
+	
+	public Product insertProduct(String name, String description, String price, String stock) {
+		errorMessage = "";
+		if(name.equals("")) {
+			errorMessage += "Name cannot be empty! ";
+		}
+		if(description.equals("")) {
+			errorMessage += "Description cannot be empty! ";
+		}
+		int intPrice = 0;
+		if(price.equals("")) {
+			errorMessage += "Price cannot be empty! ";
+		}
+		else {
+			boolean canParse = true;
+			try {
+				intPrice = Integer.parseInt(price);
+			} catch (NumberFormatException e) {
+				errorMessage += "Price must be numeric! ";
+				canParse = false;
+			}
+			if(canParse) {
+				if(intPrice < 1) {
+					errorMessage += "Price cannot be less than one! ";
+				}
+			}
+		}
+		int intStock = 0;
+		if(stock.equals("")) {
+			errorMessage += "Stock cannot be empty! ";
+		}
+		else {
+			boolean canParse = true;
+			try {
+				intStock = Integer.parseInt(stock);
+			} catch (NumberFormatException e) {
+				errorMessage += "Stock must be numeric! ";
+				canParse = false;
+			}
+			if(canParse) {
+				if(intStock < 0) {
+					errorMessage += "Stock cannot be less than zero! ";
+				}
+			}
+		}
+		
+		if(errorMessage.equals("")) {
+			product = new Product(name, description, intPrice, intStock);
+			return product.insertNewProduct();
+		}
 		return null;
 	}
 	
-	public Product insertProduct(String name, String description, int price, int stock) {
-		return null;
-	}
-	
-	public Product updateProduct(int productID, String name, String description, int price) {
+	public Product updateProduct(String productID, String name, String description, String price) {
+		errorMessage = "";
+		int intID = 0;
+		if(productID.equals("")) {
+			errorMessage += "ID cannot be empty! ";
+		}
+		else {
+			boolean canParse = true;
+			try {
+				intID = Integer.parseInt(productID);
+			} catch (NumberFormatException e) {
+				errorMessage += "ID must be numeric! ";
+				canParse = false;
+			}
+			if(canParse) {
+				Product p = getProduct(intID);
+				if(p == null) {
+					errorMessage += "ID is not exist in database! ";
+				}
+			}
+		}
+		if(name.equals("")) {
+			errorMessage += "Name cannot be empty! ";
+		}
+		if(description.equals("")) {
+			errorMessage += "Description cannot be empty! ";
+		}
+		int intPrice = 0;
+		if(price.equals("")) {
+			errorMessage += "Price cannot be empty! ";
+		}
+		else {
+			boolean canParse = true;
+			try {
+				intPrice = Integer.parseInt(price);
+			} catch (NumberFormatException e) {
+				errorMessage += "Price must be numeric! ";
+				canParse = false;
+			}
+			if(canParse) {
+				if(intPrice < 1) {
+					errorMessage += "Price cannot be less than one! ";
+				}
+			}
+		}
+		
+		if(errorMessage.equals("")) {
+			product = new Product(intID, name, description, intPrice);
+			return product.updateProduct();
+		}
 		return null;
 	}
 	
@@ -48,8 +145,33 @@ public class ProductHandler {
 		return null;
 	}
 	
-	public boolean deleteProduct(int productID) {
-		return true;
+	public boolean deleteProduct(String productID) {
+		errorMessage = "";
+		int intID = 0;
+		if(productID.equals("")) {
+			errorMessage += "ID cannot be empty! ";
+		}
+		else {
+			boolean canParse = true;
+			try {
+				intID = Integer.parseInt(productID);
+			} catch (NumberFormatException e) {
+				errorMessage += "ID must be numeric! ";
+				canParse = false;
+			}
+			if(canParse) {
+				Product p = getProduct(intID);
+				if(p == null) {
+					errorMessage += "ID is not exist in database! ";
+				}
+			}
+		}
+		
+		if(errorMessage.equals("")) {
+			product = new Product(intID);
+			return product.deleteProduct();
+		}
+		return false;
 	}
 	
 	public void viewProductManagementForm() {
