a9564651c384e58e48d839b6dbcfef56328507f8
Sendiawan Muljono <sendiawanmuljono@gmail.com>
2022-01-15 02:48:58 +0700

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,16 @@
 package main;
 
 import handlers.AuthHandler;
+import handlers.ProductHandler;
+import handlers.VoucherHandler;
 
 public class Main {
 
 	public Main() {
 		//temporary
-		AuthHandler.getInstance().viewLoginForm();
+//		AuthHandler.getInstance().viewLoginForm();
+//		ProductHandler.getInstance().viewProductManagementForm();
+		VoucherHandler.getInstance().viewVoucherManagementForm();
 	}
 
 	public static void main(String[] args) {
