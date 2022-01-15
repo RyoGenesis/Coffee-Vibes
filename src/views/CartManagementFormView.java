3f56aaf3b2fa091f52033e2ea069053908420c3b
Sendiawan Muljono <sendiawanmuljono@gmail.com>
2022-01-15 15:44:30 +0700

    Create CartManagementFormView.java

---
+++ src/views/CartManagementFormView.java
@@ -0,0 +1,21 @@
+package views;
+
+import javax.swing.JFrame;
+
+public class CartManagementFormView extends JFrame {
+
+	//**UNFINISHED**
+	
+	public CartManagementFormView() {
+		//initComp();
+		//addComp();
+		setSize(500,500);
+		setTitle("Coffee Vibes - Add To Cart");
+		//loadData();
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setVisible(true);	
+	}
+
+}
