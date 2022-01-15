0e42ec334546a885a1d19749fee3242103e5944e
Sendiawan Muljono <sendiawanmuljono@gmail.com>
2022-01-15 17:45:19 +0700

    Create AddToCartFormView.java

---
+++ src/views/AddToCartFormView.java
@@ -0,0 +1,21 @@
+package views;
+
+import javax.swing.JFrame;
+
+public class AddToCartFormView extends JFrame {
+
+	//**UNFINISHED**
+	
+	public AddToCartFormView() {
+		//initComp();
+		//addComp();
+		setSize(500,500);
+		setTitle("Coffee Vibes - Add To Cart");
+		//loadData();
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setVisible(true);	
+	}
+
+}
