4a7f1d637300ef3958285dccca656490b0e59a82
Sendiawan Muljono <sendiawanmuljono@gmail.com>
2022-01-16 17:00:57 +0700

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 import handlers.AuthHandler;
+import handlers.CartHandler;
 import handlers.EmployeeHandler;
 import handlers.ProductHandler;
 import handlers.TransactionHandler;
@@ -11,13 +12,25 @@ public class Main {
 	public Main() {
 		//temporary
 //		AuthHandler.getInstance().viewLoginForm();
-//		ProductHandler.getInstance().setUser("productAdmin");
+		
+		//VIEW YG BS DIPAKE BARISTA
+//		ProductHandler.getInstance().setUser("barista");
 //		ProductHandler.getInstance().viewProductManagementForm();
-		ProductHandler.getInstance().setUser("barista");
-		ProductHandler.getInstance().viewProductManagementForm();
 //		TransactionHandler.getInstance().setUser("barista");;
 //		TransactionHandler.getInstance().viewTransactionManagementForm();
+//		CartHandler.getInstance().viewCheckoutForm(); //ini cart management form
+		
+		//VIEW YG BS DIPAKE PRODUCT ADMIN
+//		ProductHandler.getInstance().setUser("productAdmin");
+//		ProductHandler.getInstance().viewProductManagementForm();
 //		VoucherHandler.getInstance().viewVoucherManagementForm();
+		
+		//VIEW YG BS DIPAKE MANAGER
+//		TransactionHandler.getInstance().setUser("manager");
+//		TransactionHandler.getInstance().viewTransactionManagementForm();
+//		EmployeeHandler.getInstance().viewEmployeeManagementForm("Manager");
+		
+		//VIEW YG BS DIPAKE HRD
 //		EmployeeHandler.getInstance().viewEmployeeManagementForm("Human Resource Management");
 	}
 
